9410ba5541f33930912a8e855b83d219a302eb64
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-01-05 22:10:53 +0530

    feat: add `ScrapedSearchSuggestion` struct

--- src/anime/hianime/parsers/types.rs
+++ src/anime/hianime/parsers/types.rs
@@ -1,13 +1,9 @@
-use serde::{Deserialize, Serialize};
-
 use crate::anime::hianime::types::{
-    LatestCompletedAnime, LatestEpisodeAnime, MostFavoriteAnime, MostPopularAnime, SpotlightAnime,
-    Top10AnimesWithPeriod, TopAiringAnime, TopUpcomingAnime, TrendingAnime,
+    AnimeSearchSuggestion, LatestCompletedAnime, LatestEpisodeAnime, MostFavoriteAnime,
+    MostPopularAnime, SpotlightAnime, Top10AnimesWithPeriod, TopAiringAnime, TopUpcomingAnime,
+    TrendingAnime,
 };
-
-// pub trait Jsonify {
-//     fn to_json(&self, provider_parser: &'static str) -> EnmaResult<String>;
-// }
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct ScrapedHomePage {
@@ -23,11 +19,7 @@ pub struct ScrapedHomePage {
     pub latest_completed_animes: Vec<LatestCompletedAnime>,
 }
 
-// impl Jsonify for ScrapedHomePage {
-//     fn to_json(&self, provider_parser: &'static str) -> EnmaResult<String> {
-//         match to_string(self) {
-//             Ok(stringified_json) => Ok(stringified_json),
-//             Err(_) => Err(EnmaError::json_serde_error(provider_parser, None, None)),
-//         }
-//     }
-// }
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct ScrapedSearchSuggestion {
+    pub suggestions: Vec<AnimeSearchSuggestion>,
+}
