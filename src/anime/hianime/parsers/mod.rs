b9c919cc2929f11b39fa543692cd6d78aafcf863
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-01-05 12:04:39 +0530

    feat: update hianime scraper constructor

--- src/anime/hianime/parsers/mod.rs
+++ src/anime/hianime/parsers/mod.rs
@@ -1,17 +1,14 @@
+use crate::{anime::hianime::utils::HiAnimeUtils, utils::EnmaUtils};
 use reqwest::{
     header::{HeaderMap, HeaderValue, REFERER},
     Client,
 };
 
-use crate::util::new_http_client;
+mod types;
+pub use types::*;
 
 mod home_page;
 mod search_suggestions;
-
-const SRC_BASE_URL: &str = "https://hianime.to";
-const SRC_HOME_URL: &str = "https://hianime.to/home";
-const SRC_AJAX_URL: &str = "https://hianime.to/ajax";
-const SRC_SEARCH_URL: &str = "https://hianime.to/search";
 
 #[derive(Debug)]
 pub struct Scraper {
@@ -19,12 +16,16 @@ pub struct Scraper {
 }
 
 impl Scraper {
-    pub fn new() -> Scraper {
-        let mut headers = HeaderMap::new();
-        headers.insert(REFERER, HeaderValue::from_static(SRC_BASE_URL));
+    pub fn new() -> Self {
+        let headers: HeaderMap = [(
+            REFERER,
+            HeaderValue::from_static(HiAnimeUtils::BaseUrl.value()),
+        )]
+        .into_iter()
+        .collect();
 
-        return Scraper {
-            client: new_http_client(Some(headers)),
+        return Self {
+            client: EnmaUtils::new_http_client(Some(headers)),
         };
     }
 }
