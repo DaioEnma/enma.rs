81ec8b83b273224a4d27ea9275730bc6a2da0aae
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-01-06 14:47:05 +0530

    feat: add robust error handling support

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,27 @@
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use serde_json::to_string_pretty;
 use thiserror::Error as ThisError;
 
-#[derive(Debug, Serialize, Deserialize)]
+const ANSI_ESC_CODE_COLOR_RED: &str = "\x1b[31m";
+const ANSI_ESC_CODE_COLOR_RESET: &str = "\x1b[0m";
+
+const DEFAULT_ERROR_MESSAGE: &str = "Something went wrong";
+
+/// Internal generic error implementation
+#[derive(Debug, Serialize, Deserialize, ThisError)]
+#[error(
+    "{ANSI_ESC_CODE_COLOR_RED}{{\n  \"provider_parser\": \"{}\",\n  \"message\": \"{}\",\n  \"status\": {}\n}}{ANSI_ESC_CODE_COLOR_RESET}",
+    provider_parser,
+    message,
+    status
+)]
 pub struct ErrorDetails {
     /// provider's parser was responsible for the error <br/>
     /// represented in the following pattern: `provider_name:parser_name`
     pub provider_parser: &'static str,
 
     /// error message indicating what went wrong
-    pub message: &'static str,
+    pub message: String,
 
     /// erroneous http status code
     pub status: u16,
@@ -19,17 +30,16 @@ pub struct ErrorDetails {
 impl ErrorDetails {
     fn new(
         provider_parser: &'static str,
-        err_msg: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
         const DEFAULT_ERROR_STATUS: u16 = StatusCode::INTERNAL_SERVER_ERROR.as_u16();
-        const DEFAULT_ERROR_MESSAGE: &str = "Something went wrong";
 
         let (status, message) = match (status, err_msg) {
             (Some(s), Some(msg)) => (s.as_u16(), msg),
-            (Some(s), None) => (s.as_u16(), DEFAULT_ERROR_MESSAGE),
+            (Some(s), None) => (s.as_u16(), DEFAULT_ERROR_MESSAGE.to_string()),
             (None, Some(msg)) => (DEFAULT_ERROR_STATUS, msg),
-            (None, None) => (DEFAULT_ERROR_STATUS, DEFAULT_ERROR_MESSAGE),
+            (None, None) => (DEFAULT_ERROR_STATUS, DEFAULT_ERROR_MESSAGE.to_string()),
         };
 
         let err = ErrorDetails {
@@ -38,30 +48,8 @@ impl ErrorDetails {
             status,
         };
 
-        err.log_error();
+        eprintln!("{err}");
         return err;
-    }
-
-    fn get_json(&self) -> String {
-        match to_string_pretty(self) {
-            Ok(stringified_json) => stringified_json,
-            Err(_) => {
-                println!("error occured while serializing custom error details into json");
-                "{\"status\": 500}".to_string()
-            }
-        }
-    }
-
-    fn log_error(&self) {
-        const ANSI_ESC_CODE_COLOR_RED: &str = "\x1b[31m";
-        const ANSI_ESC_CODE_COLOR_RESET: &str = "\x1b[0m";
-
-        eprintln!(
-            "{}{}{}",
-            ANSI_ESC_CODE_COLOR_RED,
-            self.get_json(),
-            ANSI_ESC_CODE_COLOR_RESET
-        );
     }
 }
 
@@ -72,92 +60,135 @@ pub type EnmaResult<T> = std::result::Result<T, EnmaError>;
 #[derive(Debug, ThisError)]
 pub enum EnmaError {
     /// represents raw source data fetch error
-    #[error("<{}>: {} SrcFetchError", details.provider_parser, details.status)]
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
     SrcFetchError { details: ErrorDetails },
 
     /// represents raw source data parse error
-    #[error("<{}>: {} SrcParseError", details.provider_parser, details.status)]
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
     SrcParseError { details: ErrorDetails },
 
     /// represents integer parsing error
-    #[error("<{}>: {} ParseIntError", details.provider_parser, details.status)]
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
     ParseIntError { details: ErrorDetails },
 
     /// represents json serializing or deserializing error
-    #[error("<{}>: {} JsonSerDeError", details.provider_parser, details.status)]
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
     JsonSerDeError { details: ErrorDetails },
 
+    /// represents any non json parsing errors
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
+    ParsingError { details: ErrorDetails },
+
     /// represents miscellaneous errors
-    #[error("<{}>: {} UnknownError", details.provider_parser, details.status)]
-    UnknownError { details: ErrorDetails },
+    #[error("{ANSI_ESC_CODE_COLOR_RED}<{}>: {} {}{ANSI_ESC_CODE_COLOR_RESET}", details.provider_parser, details.status, details.message)]
+    MiscError { details: ErrorDetails },
 }
 
 impl EnmaError {
     pub fn src_fetch_error(
         provider_parser: &'static str,
-        err_msg: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
+        const ERROR_PREFIX: &'static str = "SrcFetchError: ";
+        let err_msg =
+            Self::get_formatted_err(err_msg, ERROR_PREFIX, "Failed to fetch raw source data");
+
         return Self::SrcFetchError {
-            details: ErrorDetails::new(
-                provider_parser,
-                err_msg.or(Some("SrcFetchError: Failed to fetch source data")),
-                status,
-            ),
+            details: ErrorDetails::new(provider_parser, err_msg, status),
         };
     }
 
     pub fn src_parse_error(
         provider_parser: &'static str,
-        err_msg: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
+        const ERROR_PREFIX: &'static str = "SrcParseError: ";
+        let err_msg = Self::get_formatted_err(
+            err_msg,
+            ERROR_PREFIX,
+            "Failed to parse fetched raw source data",
+        );
+
         return Self::SrcParseError {
-            details: ErrorDetails::new(
-                provider_parser,
-                err_msg.or(Some("SrcParseError: Failed to parse fetched source data")),
-                status,
-            ),
+            details: ErrorDetails::new(provider_parser, err_msg, status),
         };
     }
 
     pub fn parse_int_error(
         provider_parser: &'static str,
-        err_msg: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
+        const ERROR_PREFIX: &'static str = "ParseIntError: ";
+        let err_msg =
+            Self::get_formatted_err(err_msg, ERROR_PREFIX, "Failed to parse integer value");
+
         return Self::ParseIntError {
-            details: ErrorDetails::new(
-                provider_parser,
-                err_msg.or(Some("ParseIntError: Failed to parse integer value")),
-                status,
-            ),
+            details: ErrorDetails::new(provider_parser, err_msg, status),
         };
     }
 
     pub fn json_serde_error(
         provider_parser: &'static str,
-        err_msg: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
+        const ERROR_PREFIX: &'static str = "JsonSerDeError: ";
+        let err_msg = Self::get_formatted_err(
+            err_msg,
+            ERROR_PREFIX,
+            "Failed to serialize or deserialize json",
+        );
+
         return Self::JsonSerDeError {
-            details: ErrorDetails::new(
-                provider_parser,
-                err_msg.or(Some(
-                    "JsonSerDeError: Failed to serialize or deserialize json",
-                )),
-                status,
-            ),
+            details: ErrorDetails::new(provider_parser, err_msg, status),
         };
     }
 
-    pub fn unknown_error(
+    pub fn parsing_error(
         provider_parser: &'static str,
-        err_message: Option<&'static str>,
+        err_msg: Option<String>,
         status: Option<StatusCode>,
     ) -> Self {
-        return Self::UnknownError {
-            details: ErrorDetails::new(provider_parser, err_message, status),
+        const ERROR_PREFIX: &'static str = "ParsingError: ";
+        let err_msg = Self::get_formatted_err(err_msg, ERROR_PREFIX, DEFAULT_ERROR_MESSAGE);
+
+        return Self::ParsingError {
+            details: ErrorDetails::new(provider_parser, err_msg, status),
         };
+    }
+
+    pub fn misc_error(
+        provider_parser: &'static str,
+        err_msg: Option<String>,
+        status: Option<StatusCode>,
+    ) -> Self {
+        const ERROR_PREFIX: &'static str = "MiscError: ";
+        let err_msg = Self::get_formatted_err(err_msg, ERROR_PREFIX, DEFAULT_ERROR_MESSAGE);
+
+        return Self::MiscError {
+            details: ErrorDetails::new(provider_parser, err_msg, status),
+        };
+    }
+
+    fn get_formatted_err(
+        err_message: Option<String>,
+        err_prefix: &'static str,
+        default_err_message: &'static str,
+    ) -> Option<String> {
+        let formatted_msg = match err_message {
+            Some(msg) => {
+                if msg.starts_with(err_prefix) {
+                    msg
+                } else {
+                    format!("{err_prefix}{msg}")
+                }
+            }
+            None => format!("{err_prefix}{default_err_message}"),
+        };
+
+        Some(formatted_msg)
     }
 }
