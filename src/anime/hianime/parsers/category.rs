94bb6d57ada658cb9f1f5e2806414c44508a8be3
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-01-07 20:37:37 +0530

    feat: add `get_category_anime` parser

---
+++ src/anime/hianime/parsers/category.rs
@@ -0,0 +1,91 @@
+use crate::{
+    anime::hianime::{
+        parsers::types::ScrapedCategory, types::ANIME_CATEGORIES, utils::HiAnimeUtils, Scraper,
+    },
+    error::EnmaResult,
+    utils::EnmaClient,
+    EnmaError,
+};
+use reqwest::StatusCode;
+use scraper::{Html, Selector};
+
+impl Scraper {
+    pub async fn get_category_anime(
+        &self,
+        category: &'static str,
+        page_number: Option<u16>,
+    ) -> EnmaResult<ScrapedCategory> {
+        const PROVIDER_PARSER: &'static str = "hianime:get_category_anime";
+
+        let category = category.trim();
+        if !ANIME_CATEGORIES.contains(category) {
+            return Err(EnmaError::misc_error(
+                PROVIDER_PARSER,
+                Some(String::from("invalid anime category")),
+                Some(StatusCode::BAD_REQUEST),
+            ));
+        }
+
+        let mut res = ScrapedCategory {
+            current_page: page_number.unwrap_or(1).max(1),
+            genres: Vec::with_capacity(41),
+            ..Default::default()
+        };
+
+        let category_name_selector =
+            &Selector::parse("#main-content .block_area .block_area-header .cat-heading").unwrap();
+        let anime_selector =
+            &Selector::parse("#main-content .tab-content .film_list-wrap .flw-item").unwrap();
+        let genre_selector = &Selector::parse(
+            "#main-sidebar .block_area.block_area_sidebar.block_area-genres .sb-genre-list li",
+        )
+        .unwrap();
+        let most_viewed_selector =
+            &Selector::parse("#main-sidebar .block_area-realtime [id^=\"top-viewed-\"]").unwrap();
+
+        let url = format!(
+            "{}/{category}?page={}",
+            HiAnimeUtils::BaseUrl.value(),
+            &res.current_page
+        );
+
+        let page = self.client.get_html(url, None, PROVIDER_PARSER).await?;
+        let document = Html::parse_document(&page);
+
+        res.category = document
+            .select(category_name_selector)
+            .next()
+            .and_then(|e| e.text().next())
+            .map(|s| s.to_string())
+            .unwrap_or(category.to_string());
+        res.total_pages = HiAnimeUtils::get_total_pages(&document);
+        res.has_next_page = HiAnimeUtils::has_next_page(&document);
+        res.animes = HiAnimeUtils::extract_animes(&document, anime_selector);
+        res.top10_animes = HiAnimeUtils::extract_top10_animes(&document, most_viewed_selector);
+        res.top10_animes = HiAnimeUtils::extract_top10_animes(&document, most_viewed_selector);
+        HiAnimeUtils::extract_genres(&document, genre_selector, &mut res.genres);
+
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::anime::hianime;
+    use serde_json::to_string_pretty;
+
+    // cargo test --lib -- anime::hianime::parsers::category::test --show-output
+    #[tokio::test]
+    async fn test_get_category_anime() {
+        let hianime = hianime::Scraper::new();
+
+        let category = "most-favorite";
+        let page_number = Some(2);
+
+        match hianime.get_category_anime(category, page_number).await {
+            // Ok(_) => (),
+            Ok(data) => println!("{}", to_string_pretty(&data).unwrap()),
+            Err(e) => eprintln!("error {}", e),
+        }
+    }
+}
