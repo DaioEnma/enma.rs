26d5910352ff24b69622a52771bc2fbade18a87a
Ritesh Ghosh <ghoshritesh55@gmail.com>
2025-01-08 13:55:56 +0530

    feat: add `get_anime_episodes` & `get_episode_servers` parsers

---
+++ src/anime/hianime/parsers/episode_servers.rs
@@ -0,0 +1,147 @@
+use crate::{
+    anime::hianime::{types::EpisodeInfo, utils::HiAnimeUtils, Scraper},
+    utils::{EnmaClient, EnmaUtils},
+    EnmaError, EnmaResult,
+};
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue, REFERER},
+    StatusCode,
+};
+use scraper::{Html, Selector};
+use serde::Deserialize;
+
+use super::ScrapedEpisodeServers;
+
+// represents the raw json data we get from the source
+#[derive(Deserialize, Debug)]
+struct RawData {
+    html: Option<String>,
+}
+
+impl Scraper {
+    pub async fn get_episode_servers(
+        &self,
+        anime_episode_id: &'static str,
+    ) -> EnmaResult<ScrapedEpisodeServers> {
+        const PROVIDER_PARSER: &'static str = "hianime:get_episode_servers";
+
+        let episode_id: &'static str = anime_episode_id
+            .trim()
+            .split("?ep=")
+            .last()
+            .unwrap_or_default();
+        if episode_id.is_empty() {
+            return Err(EnmaError::misc_error(
+                PROVIDER_PARSER,
+                Some(String::from("invalid anime episode id")),
+                Some(StatusCode::BAD_REQUEST),
+            ));
+        }
+
+        let mut res = ScrapedEpisodeServers {
+            anime_episode_id,
+            ..Default::default()
+        };
+
+        let referer = format!("{}/watch/{anime_episode_id}", HiAnimeUtils::BaseUrl.value(),);
+        let headers: HeaderMap = [
+            (REFERER, HeaderValue::from_str(&referer).unwrap()),
+            (
+                HeaderName::from_static("x-requested-with"),
+                HeaderValue::from_str(EnmaUtils::XRequestedWithHeader.value()).unwrap(),
+            ),
+        ]
+        .into_iter()
+        .collect();
+
+        let url = format!(
+            "{}?episodeId={episode_id}",
+            HiAnimeUtils::EpisodeServersUrl.value()
+        );
+
+        let episodes_no_selector = &Selector::parse(".server-notice strong b").unwrap();
+        let server_name_selector = &Selector::parse("a").unwrap();
+        let server_selector = |name: &str| {
+            return Selector::parse(&format!(
+                ".ps_-block.ps_-block-sub.servers-{name} .ps__-list .server-item"
+            ))
+            .unwrap();
+        };
+
+        let data = self
+            .client
+            .get_json::<RawData>(url, Some(headers), PROVIDER_PARSER)
+            .await?;
+
+        let document = &Html::parse_document(&data.html.unwrap_or_default().as_str());
+
+        ["sub", "dub", "raw"].map(|server| {
+            for el in document.select(&server_selector(server)) {
+                let server_id = el
+                    .attr("data-server-id")
+                    .map(|s| s.trim().parse::<u16>().ok())
+                    .flatten();
+                let server_name = el
+                    .select(server_name_selector)
+                    .next()
+                    .and_then(|el| el.text().next())
+                    .map(|s| s.to_lowercase().trim().to_string());
+
+                match server {
+                    "sub" => {
+                        res.sub.push(EpisodeInfo {
+                            server_id,
+                            server_name,
+                        });
+                    }
+                    "dub" => {
+                        res.dub.push(EpisodeInfo {
+                            server_id,
+                            server_name,
+                        });
+                    }
+                    "raw" => {
+                        res.raw.push(EpisodeInfo {
+                            server_id,
+                            server_name,
+                        });
+                    }
+                    _ => (),
+                }
+            }
+        });
+
+        res.episode_number = document
+            .select(episodes_no_selector)
+            .next()
+            .and_then(|el| el.text().next())
+            .and_then(|s| s.split(" ").last())
+            .and_then(|s| s.trim().parse::<u16>().ok());
+
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::anime::hianime;
+    use serde_json::to_string_pretty;
+
+    // cargo test --lib -- anime::hianime::parsers::episode_servers::test --show-output
+    #[tokio::test]
+    async fn test_episode_servers() {
+        let hianime = hianime::Scraper::new();
+        let anime_episode_id = "steinsgate-0-92?ep=2055";
+
+        match hianime.get_episode_servers(anime_episode_id).await {
+            // Ok(_) => (),
+            Ok(data) => {
+                println!("{}", to_string_pretty(&data).unwrap());
+
+                // assert_ne!(data.episodes.len(), 0);
+                // assert_ne!(data.total_episodes, None);
+            }
+            Err(e) => eprintln!("{}", e),
+        }
+    }
+}
---
+++ src/anime/hianime/parsers/episodes.rs
@@ -0,0 +1,111 @@
+use crate::{
+    anime::hianime::{types::AnimeEpisode, utils::HiAnimeUtils, ScrapedAnimeEpisodes, Scraper},
+    utils::{EnmaClient, EnmaUtils},
+    EnmaError, EnmaResult,
+};
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue, REFERER},
+    StatusCode,
+};
+use scraper::{Html, Selector};
+use serde::Deserialize;
+
+// represents the raw json data we get from the source
+#[derive(Deserialize, Debug)]
+struct RawData {
+    html: Option<String>,
+}
+
+impl Scraper {
+    pub async fn get_anime_episodes(
+        &self,
+        anime_id: &'static str,
+    ) -> EnmaResult<ScrapedAnimeEpisodes> {
+        const PROVIDER_PARSER: &'static str = "hianime:get_anime_episodes";
+
+        let anime_id: &'static str = anime_id.trim().split('-').last().unwrap_or_default();
+        if anime_id.is_empty() {
+            return Err(EnmaError::misc_error(
+                PROVIDER_PARSER,
+                Some(String::from("invalid anime id")),
+                Some(StatusCode::BAD_REQUEST),
+            ));
+        }
+
+        let referer = format!("{}/watch/{anime_id}", HiAnimeUtils::BaseUrl.value());
+        let headers: HeaderMap = [
+            (REFERER, HeaderValue::from_str(&referer).unwrap()),
+            (
+                HeaderName::from_static("x-requested-with"),
+                HeaderValue::from_str(EnmaUtils::XRequestedWithHeader.value()).unwrap(),
+            ),
+        ]
+        .into_iter()
+        .collect();
+        let url = format!("{}/{anime_id}", HiAnimeUtils::EpisodeListUrl.value());
+
+        let data = self
+            .client
+            .get_json::<RawData>(url, Some(headers), PROVIDER_PARSER)
+            .await?;
+
+        let document = &Html::parse_document(&data.html.unwrap_or_default().as_str());
+        let episodes_selector = &Selector::parse(".detail-infor-content .ss-list a").unwrap();
+
+        let mut res = ScrapedAnimeEpisodes::default();
+
+        res.total_episodes = match u16::try_from(document.select(episodes_selector).count()) {
+            Ok(val) => Some(val),
+            Err(_) => None,
+        };
+
+        for el in document.select(episodes_selector) {
+            let title = el.attr("title").map(|s| s.trim().to_string());
+
+            let number = el
+                .attr("data-number")
+                .map(|s| s.trim().parse::<u16>().ok())
+                .flatten();
+
+            let is_filler = el.value().classes().any(|class| class == "ssl-item-filler");
+
+            let episode_id = el
+                .attr("href")
+                .map(|s| s.split("/").last().map(|s| s.to_string()))
+                .flatten();
+
+            res.episodes.push(AnimeEpisode {
+                title,
+                number,
+                is_filler,
+                episode_id,
+            });
+        }
+
+        Ok(res)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::anime::hianime;
+    use serde_json::to_string_pretty;
+
+    // cargo test --lib -- anime::hianime::parsers::episodes::test --show-output
+    #[tokio::test]
+    async fn test_get_qtip_info() {
+        let hianime = hianime::Scraper::new();
+        let anime_id = "steinsgate-3";
+
+        match hianime.get_anime_episodes(anime_id).await {
+            // Ok(_) => (),
+            Ok(data) => {
+                println!("{}", to_string_pretty(&data).unwrap());
+
+                assert_ne!(data.episodes.len(), 0);
+                assert_ne!(data.total_episodes, None);
+            }
+            Err(e) => eprintln!("{}", e),
+        }
+    }
+}
